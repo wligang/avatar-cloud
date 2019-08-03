013f63060d5f11495390e2649437c394652c7909
Ligang.Wang <wlgchun@163.com>
2019-08-03 23:06:19 +0800

    rebuild project struts

--- avatar-upms/avatar-upms-api/src/main/java/com/wlgdo/avatar/admin/api/entity/SysRoleMenu.java
+++ avatar-upms/avatar-upms-api/src/main/java/com/wlgdo/avatar/admin/api/entity/SysRoleMenu.java
@@ -14,23 +14,32 @@
  * limitations under the License.
  */
 
-package com.wlgdo.avatar.auth;
+package com.wlgdo.avatar.admin.api.entity;
 
-
-import com.wlgdo.avatar.common.security.annotation.EnablePigFeignClients;
-import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
+ * <p>
+ * 角色菜单表
+ * </p>
+ *
  * @author lengleng
- * @date 2018年06月21日
- * 认证授权中心
+ * @since 2019/2/1
  */
-@SpringCloudApplication
-@EnablePigFeignClients
-public class PigAuthApplication {
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class SysRoleMenu extends Model<SysRoleMenu> {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PigAuthApplication.class, args);
-	}
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 角色ID
+	 */
+	private Integer roleId;
+	/**
+	 * 菜单ID
+	 */
+	private Integer menuId;
 }
