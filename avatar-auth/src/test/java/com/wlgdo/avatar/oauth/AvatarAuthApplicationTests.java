0abdfe4f7e42f0f3483031b966985eda8cc2e450
Ligang.Wang <wlgchun@163.com>
2019-08-18 23:51:49 +0800

    rebuild project struts

--- avatar-auth/src/test/java/com/wlgdo/avatar/oauth/AvatarAuthApplicationTests.java
+++ avatar-auth/src/test/java/com/wlgdo/avatar/oauth/AvatarAuthApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AvatarOauthApplicationTests {
+public class AvatarAuthApplicationTests {
 
 	@Test
 	public void contextLoads() {
