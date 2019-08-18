4a2ce970d811ccc04a34e6813f4010feb72353a3
Ligang.Wang <wlgchun@163.com>
2019-08-18 23:46:39 +0800

    rebuild project struts

---
+++ avatar-visual/avatar-codegen/src/test/java/com/wlgdo/avatar/oauth/AvatarAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wlgdo.avatar.oauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AvatarAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
